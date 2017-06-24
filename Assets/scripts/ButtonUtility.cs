cbd95923abcd31b8b5e278ea9add1c4c46f6aa01
VRRechnerRechts <baguiov@posteo.net>
2017-06-24 18:53:58 +0200

    Refactoring, erste Szenenwechsel

---
+++ Assets/scripts/ButtonUtility.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonUtility : MonoBehaviour {
+
+	public void SwitchToScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
