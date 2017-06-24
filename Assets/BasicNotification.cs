13361285cb9de9a8129f79d7e050faa4d59c9e18
Sushiy <julivizi@yahoo.de>
2017-06-24 23:18:44 +0200

    Made a very BasicNotification and also added Mediplan to take care of saving the medicine

---
+++ Assets/BasicNotification.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicNotification : MonoBehaviour
+{
+    public const int NotificationID = 1;
+
+    public string _contentTitle = "Hey!";
+    public string _contentText = "Mukki is hungry!";
+    public Texture2D LargeIcon;
+    public int _number = 1;
+    private bool _sticky = false;
+    
+    public void Notify()
+    {
+        /*
+		 * Unfortunately, from Unity 5.0, providing Android resources became obsolete.
+		 * Before Unity 5.0, you can provide the name of the drawable in the folder Plugins/Android/res/drawable.
+		 */
+        Notification notification = new Notification("food", _contentTitle, _contentText);
+        notification.SetContentInfo("By Mukowotchi9");
+        notification.EnableSound(true);
+
+        /*
+		 * Requires VIBRATE permission.
+		 */
+        notification.SetVibrate(new long[] { 200, 100, 100, 200, 200, 100 });
+
+        /*
+		 * Lights or LED notification are only working when screen is off.
+		 */
+        notification.SetLights(new Color32(255, 0, 0, 255), 500, 500);
+
+        /*
+		 * If you pass a texture, it has to be readable. 
+		 * Tick Read/Write Enabled option for the Texture in the inspector
+		 */
+        notification.SetLargeIcon(LargeIcon);
+
+        if (_number > 1)
+            notification.SetNumber(_number);
+
+        notification.SetSticky(_sticky);
+
+        NotificationManager.ShowNotification(NotificationID, notification);
+        System.DateTime time = new System.DateTime(2017, 6, 24, 22, 40, 0);
+        NotificationManager.ShowNotification(NotificationID, notification, time);
+        ++_number;
+    }
+}
---
+++ Assets/scripts/Backend/Mediplan.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MedicinePlan", menuName = "Mukowotchi/", order = 1)]
+public class Mediplan : ScriptableObject
+{
+    public struct Medicine
+    {
+        public string name;
+
+        public string description;
+
+        public List<float>[] weekdays;
+    }
+
+    private List<Medicine> medicinePlan;
+
+    public void AddMedicine(string name, string description)
+    {
+        Medicine newMed = new Medicine();
+        newMed.name = name;
+        newMed.description = description;
+
+        //Init lists for times
+        newMed.weekdays = new List<float>[7];
+        for(int i = 0; i < 7; i++)
+        {
+            newMed.weekdays[i] = new List<float>();
+        }
+    }
+
+    public void AddTime(int iMedicineIndex, int iWeekDay, int iHours, int iMinutes)
+    {
+        float fTime = iHours + iMinutes / 60;
+        List<float> tmp = medicinePlan[iMedicineIndex].weekdays[iWeekDay];
+        if (!tmp.Contains(fTime))
+            medicinePlan[iMedicineIndex].weekdays[iWeekDay].Add(fTime);
+        else
+            Debug.LogWarning( Mathf.FloorToInt(fTime) + Mathf.FloorToInt((fTime % 1.0f) * 10) +  " is already set for the given day");
+    }
+
+    public Medicine GetMedicine(int iMedicineIndex)
+    {
+        return medicinePlan[iMedicineIndex];
+    }
+
+    public string ReformatTime(float fTime)
+    {
+        float hour = Mathf.Floor(fTime);
+        float minute = (fTime - hour) * 60.0f;
+        string time = hour.ToString("00") + ":" + ((fTime - hour) * 60.0f).ToString("00");
+
+        Debug.Log(fTime + "->" + time);
+        return time;
+    }
+}
