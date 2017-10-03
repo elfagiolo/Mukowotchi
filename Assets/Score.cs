594992aaf026ebc14efb29a73db1027f940250db
hollowspecter <hollowspecder@googlemail.com>
2017-10-03 17:57:49 +0200

    menu is now dont destroy on load

---
+++ Assets/Score.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    public static Score s_instance;
+
+    void Awake()
+    {
+        if (!s_instance) s_instance = this;
+        else Destroy(transform.parent.gameObject);
+    }
+}
